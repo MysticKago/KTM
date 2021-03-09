65dfcba5f3c1bc61013e3efb2a0852b1fb88280e
MysticKago <mystickago@gmail.com>
2021-03-09 04:03:59 +0200

    3rd

--- server.js
+++ server.js
@@ -19,9 +19,9 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-if(process.env.NODE_ENV === 'production') {
-  app.use(express.static('build'));
-}
+// if(process.env.NODE_ENV === 'production') {
+//   app.use(express.static('build'));
+// }
 
 app.post('/mes', (req, res) => {
   const { email, subject, body } = req.body;
