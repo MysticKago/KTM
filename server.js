b925e7815b2383355c6a5d609cb5c82e8c7baac2
MysticKago <mystickago@gmail.com>
2021-03-09 03:46:20 +0200

    2 commit

--- server.js
+++ server.js
@@ -19,6 +19,10 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+if(process.env.NODE_ENV === 'production') {
+  app.use(express.static('build'));
+}
+
 app.post('/mes', (req, res) => {
   const { email, subject, body } = req.body;
 
